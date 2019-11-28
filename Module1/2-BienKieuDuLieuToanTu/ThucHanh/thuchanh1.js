2fab028790485d9cf6495013c93e6cd0d9bf8889
duyvu272 <nguyenduyvu27291@gmail.com>
2019-11-28 15:06:05 +0700

    update

---
+++ Module1/2-BienKieuDuLieuToanTu/ThucHanh/thuchanh1.js
@@ -0,0 +1,9 @@
+let i = 10;
+let f = 20.5;
+let b= true;
+let s = 'Nguyen Duy Vu';
+
+document.write(i+"<br>");
+document.write(f+"<br>");
+document.write(b+"<br>");
+document.write(s+"<br>");
---
+++ Module1/2-BienKieuDuLieuToanTu/ThucHanh/thuchanh2_bai1.js
@@ -0,0 +1,13 @@
+let inputWidth;
+let inputHight;
+
+inputWidth = prompt("width","input Width");
+inputHight = prompt("Hight", "input Hight");
+
+
+let w = parseInt(inputWidth);
+let h = parseInt(inputHight);
+
+let s = w*h;
+
+document.write("dien tich la : " + s);
